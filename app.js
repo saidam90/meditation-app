348ff1ec331e4ba06c26257612f7809dd5e7e3bf
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2023-03-19 01:14:32 +0400

    Change path for media files

--- app.js
+++ app.js
@@ -52,11 +52,11 @@ const app = () => {
     if (song.paused) {
       song.play();
       video.play();
-      play.src = "/svg/pause.svg";
+      play.src = "./svg/pause.svg";
     } else {
       song.pause();
       video.pause();
-      play.src = "/svg/play.svg";
+      play.src = "./svg/play.svg";
     }
   };
 
@@ -77,7 +77,7 @@ const app = () => {
     if (currentTime >= fakeDuration) {
       song.pause();
       song.currentTime = 0;
-      play.src = "/svg/play.svg";
+      play.src = "./svg/play.svg";
       video.pause();
     }
   };
